9557104d4f06b5537bfd6eec634fd02b80b3b80f
vincentmatus <61015063@kmitl.ac.th>
2019-11-27 01:20:29 +0700

    almosttt

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php 
     $conn = new mysqli('localhost','admin','1234567890','kmitl-wallet-database');
-
+    mysqli_set_charset($conn, "utf8");
     if( $conn->connect_errno){
         die("Connection failed" . $conn->connect_error);
         exit();
