0c03af4aec0f43a409f91d7aa3e706a1d8b9345b
vincentmatus <61015063@kmitl.ac.th>
2019-11-27 16:02:04 +0700

    testhost

--- register.php
+++ register.php
@@ -15,12 +15,12 @@ include_once('admin_lock.php');
     <?php
 
         include_once('connect.php');
-
+        echo $_SERVER['DOCUMENT_ROOT'];
         if(isset($_POST['submit'])){
 
             $temp = explode('.',$_FILES['fileUpload']['name']);
             $newName = round(microtime(true)).'.'. end($temp);
-            if(move_uploaded_file($_FILES['fileUpload']['tmp_name'], $_SERVER['DOCUMENT_ROOT']."/uploads/profiles/".$newName)){
+            if(move_uploaded_file($_FILES['fileUpload']['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/uploads/profiles/'.$newName)){
 
                 $sql = "INSERT INTO `user`(`UserID`, `Email`, `Password`, `name`, `surname`, `Tel.Number`,`Birthday`, `PictureAccount`)
                 VALUES ('".$_POST['userid']."', '".$_POST['email']."', MD5('".$_POST['password']."'), '".$_POST['name']."', '".$_POST['surname']."', '".$_POST['telnumber']."', '".$_POST['birthday']."', '".$newName."');";
